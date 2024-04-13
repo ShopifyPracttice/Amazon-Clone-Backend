ed3abddcea90c40fae26f6997e2881a1a5a36a9a
Muneeb <muneebehsan38@gmail.com>
2024-04-14 00:41:38 +0500

    s

--- index.js
+++ index.js
@@ -103,18 +103,14 @@ let customerId;
 let endpointSecret = "whsec_93e0c76098294832cf6a37885ce49cfc9455f0f767584123910dee4b6865020a";
 
 // app.use(express.json({verify: (req,res,buf) => { req.rawBody = buf }}))
-app.use(express.json({
-  verify: (req, res, buf) => {
-    if (req.originalUrl.startsWith('/webhook')) {
-      req.rawBody = buf.toString();
-    }
-  },
-}));
+
 
 app.post('/webhook', async (request, response) => {
   console.log(request.rawBody)
   const sig = request.headers['stripe-signature'];
-  const body = request.rawBody;
+  // const body = request.rawBody;
+  const body = JSON.stringify(request.body, null, 2);
+
   
   try {
     const event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
@@ -186,7 +182,13 @@ app.post('/webhook', async (request, response) => {
 });
 
 // app.use(bodyParser.json());
-
+app.use(express.json({
+  verify: (req, res, buf) => {
+    if (req.originalUrl.startsWith('/webhook')) {
+      req.rawBody = buf.toString();
+    }
+  },
+}));
 // app.use(cors())
 app.use(cors({
   origin: 'https://amazon-clone-front-end-tawny.vercel.app',
